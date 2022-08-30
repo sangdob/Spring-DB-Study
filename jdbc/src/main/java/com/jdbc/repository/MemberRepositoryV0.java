83d3c6fc5fb11a7d06d61ce5d0858a0ad7428eb6
sangdob <51118110+sangdob@users.noreply.github.com>
2022-08-31 01:01:23 +0900

    Spring database study
    member repository jdbc save model

--- jdbc/src/main/java/com/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/com/jdbc/repository/MemberRepositoryV0.java
@@ -4,9 +4,7 @@ import com.jdbc.connect.DBConnectionUtil;
 import com.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 @Slf4j
 public class MemberRepositoryV0 {
@@ -22,13 +20,42 @@ public class MemberRepositoryV0 {
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, member.getMemberId());
             pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
             return member;
         } catch (SQLException e) {
             e.printStackTrace();
             throw e;
         } finally {
-            pstmt.close();
-            con.close();
+            close(con, pstmt, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs){
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error ", e);
+                e.printStackTrace();
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error ", e);
+                e.printStackTrace();
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error ", e);
+                e.printStackTrace();
+            }
         }
     }
 
