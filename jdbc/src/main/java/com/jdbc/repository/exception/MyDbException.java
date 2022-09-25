b7cf67e83b9e8146dd96cd55497349f52e0a5632
sangdob <51118110+sangdob@users.noreply.github.com>
2022-09-25 22:13:33 +0900

    Spring database study
    exception class log add

--- jdbc/src/main/java/com/jdbc/repository/exception/MyDbException.java
+++ jdbc/src/main/java/com/jdbc/repository/exception/MyDbException.java
@@ -1,22 +1,31 @@
 package com.jdbc.repository.exception;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MyDbException extends RuntimeException{
     public MyDbException() {
+        super();
+        log.info("myDbException error");
     }
 
     public MyDbException(String message) {
         super(message);
+        log.info("myDbException error");
     }
 
     public MyDbException(String message, Throwable cause) {
         super(message, cause);
+        log.info("myDbException error");
     }
 
     public MyDbException(Throwable cause) {
         super(cause);
+        log.info("myDbException error");
     }
 
     public MyDbException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+        log.info("myDbException error");
     }
 }
